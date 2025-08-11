4c076082641f09af673770ff5e924f3e10eebc35
Divay_Jain <divayjain1208@gmail.com>
2025-08-11 19:43:36 +0530

    Add files via upload

---
+++ Day3/q17/script.js
@@ -0,0 +1,8 @@
+
+let name = "XYZ";
+let age = 19;
+let city = "ABC";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`City: ${city}`);
---
+++ Day3/q24/script.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "XYZ",
+    age: 19,
+    city: "ABC"
+};
+
+
+console.log("Name: " + person.name);
+console.log("Age: " + person.age);
+console.log("City: " + person.city);
